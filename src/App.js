34c5e3cc1df1cc27156a53a253565bbdac660f43
justincampbell <campbelljustin451@gmail.com>
2024-04-23 15:03:08 -0500

    fix: finsh weather

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState, useRef,useEffect,Fragment } from 'react';
+import CurrentLocation from "./currentLocation";
 
 export default function App() {   
 	return (
 		<Fragment>
 			<div className='container'>
-
+				<CurrentLocation />
 			</div>
 		</Fragment>
 	)
---
+++ src/apiKeys.js
@@ -0,0 +1,8 @@
+ const apiKeys = {
+	key: "a717ae6d34c9d905fa937c1699fe23a4",
+	base: "https://api.openweathermap.org/data/2.5/",
+ };
+ 
+ // Export the API keys configuration object
+ export default apiKeys;
+ 
