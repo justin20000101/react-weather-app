2ee07ac98793e36260d6a7346eb5a05e71fcc683
justincampbell <campbelljustin451@gmail.com>
2024-04-23 11:34:12 -0500

    fix: init app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState, useRef,useEffect,Fragment } from 'react';
+import { findRenderedDOMComponentWithTag } from 'react-dom/test-utils';
+
+
+export default function App() {   
+	return (
+		<Fragment>
+			<h2>
+				Hello world! Justin campbell
+			</h2>
+		</Fragment>
+	)
+}
+
